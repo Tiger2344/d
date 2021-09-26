1d6947e9c059a24a675a89fd53d7c30e2fe6bcda
Tiger2344 <90754172+Tiger2344@users.noreply.github.com>
2021-09-26 14:19:08 +0800

    Add files via upload

--- 弟子对决.c
+++ 弟子对决.c
@@ -4,10 +4,35 @@ int main()
 {
 	
 	int x,y;
-	int t=1 ;
+	int t=1;
 	int m=0,n=0;
+	printf("弟子对决\n");
+	
 	while(t<=3){
+		
 		scanf("%d %d  ",&x,&y);
+		int k,m,n,g;
+		printf("是否使用技能(是请输入一，不是请输入零)\n");
+		scanf("%d %d %d %d",&k,&m,&n,&g) ;
+		
+		if(k==1){
+			printf("要有理想\n");
+			x=x+2;
+		}
+		if(m==1){
+			printf("不讲武德\n");
+			x=x-2;
+		}
+		if(n==1){
+			printf("如来神掌\n");
+			x=6;
+			
+		}
+		if(g==1){
+			printf("铠甲合体\n");
+			x=y;
+		}
+		
 		if(x>y){
 			printf("红方胜\n");
 			m++;
